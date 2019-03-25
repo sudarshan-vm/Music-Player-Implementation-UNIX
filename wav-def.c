514352d2a45fe69e5271f72603e12bfd82c72f2c
Sudarshan VM <sudarshan.vm2000@gmail.com>
2019-03-25 22:56:45 +0530

    modification of def file

--- wav-def.c
+++ wav-def.c
@@ -1,23 +1,5 @@
 #include"wav.h"
 
-void readfile()
-{
-    int16_t *samples = NULL;
-    
-    wavread("sound.wav",&samples);
-    printf("No. of channels: %d\n",header->num_channels);
-    printf("Sample rate:     %d\n",header->sample_rate);
-    printf("Bit rate:        %dkbps\n",header->byte_rate*8 / 1000);
-    printf("Bits per sample: %d\n\n",header->bps);
-    printf("Sample 0:        %d\n",samples[0]);
-    printf("Sample 1:        %d\n",samples[1]);
-   // Modify the header values & samples before writing the new file
-    wavwrite("sound.wav",samples);
-    free(header);
-    free(samples);
-
-}
-
 void playAudio()
 {
 
@@ -66,4 +48,23 @@ void wavwrite(char *file_name, int16_t *samples)
         close(fd);
     }
     
+
+void readfile()
+{
+    int16_t *samples = NULL;
+    
+    wavread("sound.wav",&samples);
+    printf("No. of channels: %d\n",header->num_channels);
+    printf("Sample rate:     %d\n",header->sample_rate);
+    printf("Bit rate:        %dkbps\n",header->byte_rate*8 / 1000);
+    printf("Bits per sample: %d\n\n",header->bps);
+    printf("Sample 0:        %d\n",samples[0]);
+    printf("Sample 1:        %d\n",samples[1]);
+   // Modify the header values & samples before writing the new file
+    wavwrite("sound.wav",samples);
+    free(header);
+    free(samples);
+
+}
+
 
