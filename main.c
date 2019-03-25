d1983f52d6dc92cbe179a390f390b7362c81dc27
Sudarshan VM <sudarshan.vm2000@gmail.com>
2019-03-25 19:44:05 +0530

    ..

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
 #include"wav.h"
+
 int main()
 {
-    int16_t *samples = NULL;
-    
-    
-    wavread("sound.wav",&samples);
-    printf("No. of channels: %d\n",header->num_channels);
-    printf("Sample rate:     %d\n",header->sample_rate);
-    printf("Bit rate:        %dkbps\n",header->byte_rate*8 / 1000);
-    printf("Bits per sample: %d\n\n",header->bps);
-    printf("Sample 0:        %d\n",samples[0]);
-    printf("Sample 1:        %d\n",samples[1]);
-   // Modify the header values & samples before writing the new file
-    wavwrite("sound.wav",samples);
-    free(header);
-    free(samples);
-
+		int ch;
+		
+		printf("\nEnter a choice: ");
+		scanf("%d",&ch);
+		
+		switch(ch)
+		{
+			case 1: printf("\nReading the audio-file...\n");
+					readfile();
+					break;
+			case 2: printf("\nPlaying audio-file...\n");
+					playAudio();
+					break;
+			default: printf("\nInvalid input.\n");
+					break;		
+		}
 	return 0;
 }
+
